ce1ddeb15009023f12242aa3d5c73a431f3e5cd4
Assylbekov Yersultan <fitsyersi@gmail.com>
2019-09-16 18:35:48 +0200

    16.09.2019 3.1

---
+++ app/src/main/java/kz/newsapp/model/ItemViewModel.java
@@ -0,0 +1,29 @@
+package kz.newsapp.model;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModel;
+import android.arch.paging.LivePagedListBuilder;
+import android.arch.paging.PageKeyedDataSource;
+import android.arch.paging.PagedList;
+import android.content.ClipData;
+import kz.newsapp.ItemDataSourceFactory;
+import kz.newsapp.date.ItemDataSource;
+
+public class ItemViewModel extends ViewModel {
+
+     public LiveData<PagedList<Article>> itemPagedList;
+    public LiveData<PageKeyedDataSource<Integer,Article>> liveDataSource;
+
+    public ItemViewModel(){
+
+        ItemDataSourceFactory itemDataSourceFactory = new ItemDataSourceFactory();
+        liveDataSource = itemDataSourceFactory.getItemLiveDataSource();
+
+        PagedList.Config config =
+                (new PagedList.Config.Builder()).setEnablePlaceholders(false)
+                .setPageSize(ItemDataSource.PAGE_SIZE).build();
+
+        itemPagedList = (new LivePagedListBuilder(itemDataSourceFactory, config)).build();
+    }
+
+}
